b237c6350824489538d596849f2ae5c145315b90
Thomas McDonald <4001137@keranna.qc.ca>
2024-01-01 01:49:05 -0500

    Added Drink rating GUI and more file organization

---
+++ test_gui/drink_rating_gui/target_drink.gd
@@ -0,0 +1,20 @@
+extends Panel
+
+
+func _on_temp_slider_value_changed(value):
+	$VBox/TempSliderContainer/TempValue.text = str(value)
+
+func _on_astringency_slider_value_changed(value):
+	$VBox/AstringencySliderContainer/AstringencyValue.text = str(value)
+
+func _on_sweetness_slider_value_changed(value):
+	$VBox/SweetnessSliderContainer/SweetnessValue.text = str(value)
+
+func _on_florality_slider_value_changed(value):
+	$VBox/FloralitySliderContainer/FloralityValue.text = str(value)
+
+func _on_spicedness_slider_value_changed(value):
+	$VBox/SpicednessSliderContainer/SpicednessValue.text = str(value)
+
+func _on_nuttyness_slider_value_changed(value):
+	$VBox/NuttynessSliderContainer/NuttynessValue.text = str(value)
