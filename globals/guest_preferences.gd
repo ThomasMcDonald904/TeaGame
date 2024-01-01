0aeea7a024bb51622dd59c2012bb6b5cc939dda6
Thomas McDonald <4001137@keranna.qc.ca>
2024-01-01 10:32:31 -0500

    Fixed merge issues

--- globals/guest_preferences.gd
+++ globals/guest_preferences.gd
@@ -1,14 +1,62 @@
 class_name GuestPreferences extends DrinkProperties
 
-var temperature_tolerance_lower_bound
-var temperature_tolerance_upper_bound
-var astringency_tolerance_lower_bound
-var astringency_tolerance_upper_bound
-var sweetness_tolerance_lower_bound
-var sweetness_tolerance_upper_bound
-var florality_tolerance_lower_bound
-var florality_tolerance_upper_bound
-var spicedness_tolerance_lower_bound
-var spicedness_tolerance_upper_bound
-var nuttyness_tolerance_lower_bound
-var nuttyness_tolerance_upper_bound
+var temperature_tolerance_lower_bound: float:
+	set(value):
+		temperature_tolerance_lower_bound = value
+	get:
+		return temperature_tolerance_lower_bound
+var temperature_tolerance_upper_bound: float:
+	set(value):
+		temperature_tolerance_upper_bound = value
+	get:
+		return temperature_tolerance_upper_bound
+var astringency_tolerance_lower_bound: float:
+	set(value):
+		astringency_tolerance_lower_bound = value
+	get:
+		return astringency_tolerance_lower_bound
+var astringency_tolerance_upper_bound: float:
+	set(value):
+		astringency_tolerance_upper_bound = value
+	get:
+		return astringency_tolerance_upper_bound
+var sweetness_tolerance_lower_bound: float:
+	set(value):
+		sweetness_tolerance_lower_bound = value
+	get:
+		return sweetness_tolerance_lower_bound
+var sweetness_tolerance_upper_bound: float:
+	set(value):
+		sweetness_tolerance_upper_bound = value
+	get:
+		return sweetness_tolerance_upper_bound
+var florality_tolerance_lower_bound: float:
+	set(value):
+		florality_tolerance_lower_bound = value
+	get:
+		return florality_tolerance_lower_bound
+var florality_tolerance_upper_bound: float:
+	set(value):
+		florality_tolerance_upper_bound = value
+	get:
+		return florality_tolerance_upper_bound
+var spicedness_tolerance_lower_bound: float:
+	set(value):
+		spicedness_tolerance_lower_bound = value
+	get:
+		return spicedness_tolerance_lower_bound
+var spicedness_tolerance_upper_bound: float:
+	set(value):
+		spicedness_tolerance_upper_bound = value
+	get:
+		return spicedness_tolerance_upper_bound
+var nuttyness_tolerance_lower_bound: float:
+	set(value):
+		nuttyness_tolerance_lower_bound = value
+	get:
+		return nuttyness_tolerance_lower_bound
+var nuttyness_tolerance_upper_bound: float:
+	set(value):
+		nuttyness_tolerance_upper_bound = value
+	get:
+		return nuttyness_tolerance_upper_bound
