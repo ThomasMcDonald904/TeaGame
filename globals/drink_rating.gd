f70b236c379c2dac10efc2778b1e7866d94167fd
Thomas McDonald <4001137@keranna.qc.ca>
2024-01-01 00:57:18 -0500

    added get drink rating function

--- globals/drink_rating.gd
+++ globals/drink_rating.gd
@@ -1,7 +1,12 @@
 func get_rating(guest_preferences: GuestPreferences, served_drink: DrinkProperties):
 	var score = 0
 	# TODO finish rating for all properties
-	score += get_property_rating(served_drink.get("temperature"), guest_preferences.get("astringency"), )
+	score += get_property_rating(served_drink.get("temperature"), guest_preferences.get("temperature"), guest_preferences.get("temperature_tolerance_lower_bound"), guest_preferences.get("temperature_tolerance_upper_bound"))
+	score += get_property_rating(served_drink.get("astringency"), guest_preferences.get("astringency"), guest_preferences.get("astringency_tolerance_lower_bound"), guest_preferences.get("astringency_tolerance_upper_bound"))
+	score += get_property_rating(served_drink.get("sweetness"), guest_preferences.get("sweetness"), guest_preferences.get("sweetness_tolerance_lower_bound"), guest_preferences.get("sweetness_tolerance_upper_bound"))
+	score += get_property_rating(served_drink.get("florality"), guest_preferences.get("florality"), guest_preferences.get("florality_tolerance_lower_bound"), guest_preferences.get("florality_tolerance_upper_bound"))
+	score += get_property_rating(served_drink.get("spicedness"), guest_preferences.get("spicedness"), guest_preferences.get("spicedness_tolerance_lower_bound"), guest_preferences.get("spicedness_tolerance_upper_bound"))
+	score += get_property_rating(served_drink.get("nuttyness"), guest_preferences.get("nuttyness"), guest_preferences.get("nuttyness_tolerance_lower_bound"), guest_preferences.get("nuttyness_tolerance_upper_bound"))
 
 func get_property_rating(served, target, threshold_lower_bound, threshold_upper_bound):
 	if served < target:
