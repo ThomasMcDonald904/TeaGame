a7e3f79bcbeed71970df6405ba51388a3affccec
Benjamin McDonald <streetsamurai2001@gmail.com>
2023-12-31 21:53:22 -0500

    Added game state global
    Using an AnimationTree the game state can be controlled.

---
+++ scripts/game_state.gd
@@ -0,0 +1,15 @@
+extends AnimationTree
+
+signal enter_main_menu()
+signal enter_brewdio()
+
+@export var start : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
