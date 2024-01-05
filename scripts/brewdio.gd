5f708c73c4bd5dc2a9910ec8a35c742e71318ad8
Thomas McDonald <4001137@keranna.qc.ca>
2024-01-04 22:51:05 -0500

    Added service tassle

--- scripts/brewdio.gd
+++ scripts/brewdio.gd
@@ -4,10 +4,14 @@ func _ready():
 	$Window.window_clicked.connect(next_day)
 
 func next_day(nbr_days):
-	$Control/VBoxContainer/RichTextLabel.visible = true if nbr_days == 1 else false
-	$Control/VBoxContainer/Title.visible = false if nbr_days == 1 else true
-	$Control/VBoxContainer/nbr_nights.visible = false if nbr_days == 1 else true
-	$Control/AnimationPlayer.play("fade_to_night")
+	$Control/VBoxContainer/GuestsArrivingTomorrow.visible = true if nbr_days == 1 else false
+	$Control/VBoxContainer/Title.visible = false if nbr_days == 1 or nbr_days == 0 else true
+	$Control/VBoxContainer/nbr_nights.visible = false if nbr_days == 1 or nbr_days == 0 else true
+	$Control/VBoxContainer/NewLetter.visible = true if nbr_days == 0 else false
+	if nbr_days == -1:
+		$Control/AnimationPlayer.play("fade_to_night_new_week")
+	else:
+		$Control/AnimationPlayer.play("fade_to_night")
 	$Control/VBoxContainer/nbr_nights.text = "[center]" + str(nbr_days)
 
 func increment_current_day():
