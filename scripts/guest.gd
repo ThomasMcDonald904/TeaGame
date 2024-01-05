38722d262401e1ddbf8fbaa454ca6bcf71ef5946
M3ALW0RM <streetsamurai2001@gmail.com>
2024-01-05 16:57:22 -0500

    Added guest drink animation

--- scripts/guest.gd
+++ scripts/guest.gd
@@ -10,7 +10,7 @@ extends Node2D
 @export var scarf_offset: int
 
 func _ready():
-	var profile_sprite: Sprite2D = Sprite2D.new()
+	var profile_sprite: Sprite2D = $ProfileSprite
 	var hat_sprite: Sprite2D = Sprite2D.new()
 	var glasses_sprite: Sprite2D = Sprite2D.new()
 	var scarf_sprite: Sprite2D = Sprite2D.new()
@@ -21,27 +21,27 @@ func _ready():
 	hat_sprite.position.y += hat_offset
 	glasses_sprite.position.y += glasses_offset
 	scarf_sprite.position.y += scarf_offset
-	add_child(profile_sprite)
 	add_child(hat_sprite)
 	add_child(glasses_sprite)
 	add_child(scarf_sprite)
 
 func _process(delta):
-	if Input.is_action_just_pressed("click"):
-		for child in get_children():
-			child.queue_free()
-		var profile_sprite: Sprite2D = Sprite2D.new()
-		var hat_sprite: Sprite2D = Sprite2D.new()
-		var glasses_sprite: Sprite2D = Sprite2D.new()
-		var scarf_sprite: Sprite2D = Sprite2D.new()
-		profile_sprite.texture = guest_profiles.pick_random()
-		hat_sprite.texture = hat_accessories.pick_random()
-		glasses_sprite.texture = glasses_accessories.pick_random()
-		scarf_sprite.texture = scarf_accessories.pick_random()
-		hat_sprite.position.y += hat_offset
-		glasses_sprite.position.y += glasses_offset
-		scarf_sprite.position.y += scarf_offset
-		add_child(profile_sprite)
-		add_child(hat_sprite)
-		add_child(glasses_sprite)
-		add_child(scarf_sprite)
+	#if Input.is_action_just_pressed("click"):
+	#	for child in get_children():
+	#		child.queue_free()
+	#	var profile_sprite: Sprite2D = Sprite2D.new()
+	#	var hat_sprite: Sprite2D = Sprite2D.new()
+	#	var glasses_sprite: Sprite2D = Sprite2D.new()
+	#	var scarf_sprite: Sprite2D = Sprite2D.new()
+	#	profile_sprite.texture = guest_profiles.pick_random()
+	#	hat_sprite.texture = hat_accessories.pick_random()
+	#	glasses_sprite.texture = glasses_accessories.pick_random()
+	#	scarf_sprite.texture = scarf_accessories.pick_random()
+	#	hat_sprite.position.y += hat_offset
+	#	glasses_sprite.position.y += glasses_offset
+	#	scarf_sprite.position.y += scarf_offset
+	#	add_child(profile_sprite)
+	#	add_child(hat_sprite)
+	#	add_child(glasses_sprite)
+	#	add_child(scarf_sprite)
+	pass
--- scripts/tea_room.gd
+++ scripts/tea_room.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var brew: Brew
 
+var show_guests: bool = false
+var fill_cups: bool = false
+
 func _ready():
 	#guests have not yet arived
 	if Globals.preparation_days - (Globals.current_day % (Globals.preparation_days + 1)) != 0:
@@ -12,3 +15,11 @@ func _ready():
 	else:
 		$Control/RoomClosed.visible = false
 		$BrewingStation.visible = true
+		show_guests = true
+
+
+func _on_brewing_station_serve_brew():
+	fill_cups = true
+
+func _on_tea_drunk():
+	GameState.tea_session_done = true
