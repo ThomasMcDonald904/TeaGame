bdbe051f1ce1b8696cdb092ab6648fd3e182f34b
Thomas McDonald <4001137@keranna.qc.ca>
2024-01-05 12:01:50 -0500

    Added brewdio-to-office door and made letter stay the same till new letter

--- scripts/office.gd
+++ scripts/office.gd
@@ -5,9 +5,13 @@ func _ready():
 	$AnimationPlayer.animation_finished.connect(func(_anim_name): GameState.letter_read = true)
 
 func give_letter():
-	var storyteller: Storyteller = Storyteller.new()
-	var guest_profile: GuestProfile = GuestProfile.new()
-	storyteller.generate_story(guest_profile)
-	Globals.current_guest_preferences = guest_profile
-	$Control/ColorRect/RichTextLabel.text = guest_profile.story
-	$AnimationPlayer.play("ReadLetter")
+	if GameState.first_time_experience or Globals.preparation_days - (Globals.current_day % (Globals.preparation_days + 1)) == 0:
+		var storyteller: Storyteller = Storyteller.new()
+		var guest_profile: GuestProfile = GuestProfile.new()
+		storyteller.generate_story(guest_profile)
+		Globals.current_guest_preferences = guest_profile
+		$Control/ColorRect/RichTextLabel.text = guest_profile.story
+		$AnimationPlayer.play("ReadLetter")
+	else:
+		$Control/ColorRect/RichTextLabel.text = Globals.current_guest_preferences.story
+		$AnimationPlayer.play("ReadLetter")
