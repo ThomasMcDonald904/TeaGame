ede8bb9619e2e111a0acf2ff6d5e05f6286e4b97
Thomas McDonald <4001137@keranna.qc.ca>
2024-03-10 00:23:25 -0500

    Flavour Graph now increases when hovering
    Needs to offset slightly to the center, not just increase

--- scripts/flavour_particle_debug.gd
+++ scripts/flavour_particle_debug.gd
@@ -10,7 +10,6 @@ func _on_spawn_particles_button_up():
 	for item: Item in $Items.get_children():
 		# Normally the manager shouldn't call the particle's signal, 
 		# it should call itself, ensuring decoupling
-		var test = item.ingredient.flavor_queue
 		if queue_counter <= item.ingredient.flavor_queue.size() - 1:
 			item.emit_particles.emit(item.global_position, item.ingredient.flavor_queue[queue_counter].normal_flavor_intensity_value, item.ingredient.flavor_queue[queue_counter].normal_flavor)
 	queue_counter += 1
--- scripts/particle_manager.gd
+++ scripts/particle_manager.gd
@@ -1,18 +1,18 @@
-extends Node
+extends Node2D
 
 var FLAVOUR_PARTICLE_PS: PackedScene = preload("res://scenes/flavour_particle.tscn")
 
 @onready var drink_contents: DrinkContents = DrinkContents.new()
 
 @onready var flavour_graphs = {
-	DrinkProperty.PropertyType.ASTRINGENCY: $GraphAstringency,
-	DrinkProperty.PropertyType.SWEETNESS: $GraphSweetness,
-	DrinkProperty.PropertyType.FLORALITY: $GraphFlorality,
-	DrinkProperty.PropertyType.SPICEDNESS: $GraphSpicedness,
-	DrinkProperty.PropertyType.NUTTYNESS: $GraphNuttyness
+	DrinkProperty.PropertyType.ASTRINGENCY: [$GraphAstringency, Vector2(0, 0)],
+	DrinkProperty.PropertyType.SWEETNESS: [$GraphSweetness, Vector2(0, 0)],
+	DrinkProperty.PropertyType.FLORALITY: [$GraphFlorality, Vector2(0, 0)],
+	DrinkProperty.PropertyType.SPICEDNESS: [$GraphSpicedness, Vector2(0, 0)],
+	DrinkProperty.PropertyType.NUTTYNESS: [$GraphNuttyness, Vector2(0, 0)]
 }
 
-var spawn_radius = 20
+var spawn_radius = 30
 
 func spawn_particles(wanted_global_position: Vector2, number_of_particles_to_emit: int, flavour: DrinkProperty.PropertyType):
 	for i in range(number_of_particles_to_emit):
@@ -29,16 +29,18 @@ func divide_flavour_graph():
 	if drink_contents.get_total_number_of_particles() == 1:
 		for flavour in drink_contents.flavour_particle_quantity.keys():
 			var fill_angle = 360 * drink_contents.flavour_particle_quantity[flavour]
-			flavour_graphs[flavour].radial_initial_angle = 0
-			flavour_graphs[flavour].radial_fill_degrees = fill_angle
+			flavour_graphs[flavour][0].radial_initial_angle = 0
+			flavour_graphs[flavour][0].radial_fill_degrees = fill_angle
+			flavour_graphs[flavour][1] = Vector2(0, fill_angle)
 	else: 
 		var angle_per_particle: float = 360.0 / drink_contents.get_total_number_of_particles()
 		for flavour in drink_contents.flavour_particle_quantity.keys():
 			var fill_angle = angle_per_particle * drink_contents.flavour_particle_quantity[flavour]
 			var initial_angle_tweener = get_tree().create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 			var fill_degrees_tweener = get_tree().create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-			initial_angle_tweener.tween_property(flavour_graphs[flavour], "radial_initial_angle", previous_angle, 0.7)
-			fill_degrees_tweener.tween_property(flavour_graphs[flavour], "radial_fill_degrees", fill_angle, 0.7)
+			initial_angle_tweener.tween_property(flavour_graphs[flavour][0], "radial_initial_angle", previous_angle, 0.7)
+			fill_degrees_tweener.tween_property(flavour_graphs[flavour][0], "radial_fill_degrees", fill_angle, 0.7)
+			flavour_graphs[flavour][1] = Vector2(previous_angle, fill_angle + previous_angle)
 			previous_angle += fill_angle
 
 
@@ -46,3 +48,22 @@ func _on_area_2d_body_entered(flavour_particle: FlavourParticle):
 	drink_contents.flavour_particle_quantity[flavour_particle.represented_flavour] += 1
 	divide_flavour_graph()
 	flavour_particle.queue_free()
+
+
+func _on_area_2d_mouse_entered():
+	# This chunk of math which is surprisingly hard to do gives the angle between 
+	# the mouse cursor and the positive y axis
+	var angle_rad = atan2(-(get_window().get_mouse_position().x - position.x), -(get_window().get_mouse_position().y - position.y))
+	var angle_degrees = 360 - (rad_to_deg(angle_rad) + 360 if angle_rad < 0 else rad_to_deg(angle_rad))
+	for graph in flavour_graphs.values():
+		if angle_degrees >= graph[1].x and angle_degrees < graph[1].y:
+			graph[0].scale = Vector2(1.1, 1.1)
+			graph[0].position = Vector2(graph[0].position.x * graph[0].scale.x, graph[0].position.y * graph[0].scale.y)
+			graph[0].set_anchors_preset(Control.PRESET_CENTER)
+
+
+func _on_area_2d_mouse_exited():
+	for graph in flavour_graphs.values():
+		graph[0].position = Vector2(graph[0].position.x / graph[0].scale.x, graph[0].position.y / graph[0].scale.y)
+		graph[0].scale = Vector2(1, 1)
+		graph[0].set_anchors_preset(Control.PRESET_CENTER)
